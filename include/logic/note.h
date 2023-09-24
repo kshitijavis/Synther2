49dd8c86c61a4452b5cf8d2916cdecfc6d5d49c9
Kshitij Sinha <kshitij.avi.s@gmail.com>
2023-09-24 15:35:48 -0500

    Rorganize directories

---
+++ include/logic/note.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace synther {
+
+class Note {
+ public:
+  int num;
+  Note(int);
+
+};
+
+}
---
+++ src/logic/note.cpp
@@ -0,0 +1,7 @@
+#include "logic/note.h"
+
+using synther::Note;
+
+Note::Note(int n) {
+  num = n;
+}
